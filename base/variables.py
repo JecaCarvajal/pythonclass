d622a5feb5debf5c7705b4df4db31b4d9530c72a
Jaime Eduardo Carvajal Amado <jamado@novarihealth.com>
2026-08-22 18:56:26 -0500

    primeros pasos

---
+++ base/variables.py
@@ -0,0 +1,32 @@
+#snake_case 
+#mi_variable
+
+mi_texto = "Hola mundo"
+mi_texto2 = "Hola mundo"
+mi_texto3 = mi_texto2
+mi_numero = 18
+mi_float = 3.14
+mi_booleano = True
+mi_texto_h = mi_texto[0]
+mi_texto_h = mi_texto[0:4]
+
+print(mi_texto)
+print(mi_texto_h)
+print(mi_numero)
+print(mi_float)
+print(mi_booleano)
+print(type(mi_texto))
+
+mi_texto = 5
+print(type(mi_texto))
+
+print(mi_texto3)
+mi_texto3 = "Hola mundo 2"
+print(mi_texto3)
+print(mi_texto2)
+print(mi_texto==mi_texto2)
+print(1+2)
+print(mi_numero+mi_float)
+print(mi_texto2+mi_texto3)
+print(mi_texto2*3)
+print("Samuel"*18)
